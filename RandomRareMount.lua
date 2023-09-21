d00667b2450c332aa714163145bf581062e8cfcd
Sören Gade <gade.soeren@gmail.com>
2023-09-21 03:23:52 +0200

    Add ability to summon from all mounts

--- RandomRareMount.lua
+++ RandomRareMount.lua
@@ -17,32 +17,57 @@ end
 
 ---@alias ShortMountInfo { name: string, mountId: number, spellId: number }
 
----Returns the player's favorite mounts from the mount journal.
----@return ShortMountInfo[]
-function RandomRareMountAddon:GetFavoriteMounts()
+---@param filter fun(name: string, spellID: number, isActive: boolean, sourceType: number, isFavorite: boolean, isFactionSpecific: boolean, faction: number?, isForDragonriding: boolean): boolean
+function RandomRareMountAddon:GetMounts(filter)
     ---@type ShortMountInfo[]
-    local favoriteMounts = {}
+    local selectedMounts = {}
+    if type(filter) ~= "function" then
+        return selectedMounts
+    end
+    local journal = C_MountJournal
 
-    local mountIds = C_MountJournal.GetMountIDs();
-    for i, mountId in ipairs(mountIds) do
-        local name, spellId, _, _, isUsable, _, isFavorite, _, _, shouldHideOnChar, isCollected, _, _ = C_MountJournal.GetMountInfoByID(mountId);
+    local mountIDs = journal.GetMountIDs()
 
-        if ( isCollected and shouldHideOnChar ~= true and isUsable and isFavorite ) then
-            ---@type ShortMountInfo
-            local mountInfo = {}
-            mountInfo.name = name
-            mountInfo.mountId = mountId
-            mountInfo.spellId = spellId
-            table.insert(favoriteMounts, mountInfo)
+    for _, mountID in ipairs(mountIDs) do
+        local name, spellID, _, isActive, isUsable, sourceType, isFavorite, isFactionSpecific, faction, shouldHideOnChar, isCollected, _, isForDragonriding = journal.GetMountInfoByID(mountID)
+
+        if isUsable and shouldHideOnChar ~= true and isCollected then
+            local couldSelect = filter(name, spellID, isActive, sourceType, isFavorite, isFactionSpecific, faction, isForDragonriding)
+
+            if couldSelect then
+                ---@type ShortMountInfo
+                local mountInfo = {}
+                mountInfo.name = name
+                mountInfo.mountId = mountID
+                mountInfo.spellId = spellID
+                table.insert(selectedMounts, mountInfo)
+            end
         end
     end
 
-    return favoriteMounts
+    return selectedMounts
+end
+
+function RandomRareMountAddon:GetAllMounts()
+    local filter = function (_, _, isActive, _, _, _, _, _)
+        return isActive ~= true
+    end
+
+    return self:GetMounts(filter)
+end
+
+---Returns the player's favorite mounts from the mount journal.
+function RandomRareMountAddon:GetFavoriteMounts()
+    local filter = function (_, _, isActive, _, isFavorite, _, _, _)
+        return isActive ~= true and isFavorite
+    end
+
+    return self:GetMounts(filter)
 end
 
 ---Calculates a weighted random index for the given array.
 ---@param favoriteMountsWithRarities { mountInfo: ShortMountInfo, rarity: number, invertedRarity: number }[]
----@return integer | nil
+---@return integer | nil, number|nil
 function RandomRareMountAddon:WeightedRandomSelect(favoriteMountsWithRarities)
     -- based on https://gist.github.com/TeoTwawki/87d5dab7e4515f4a2981df7ea8e0a798
     -- thanks @ncg for the help in fine-tuning this!
@@ -59,24 +84,23 @@ function RandomRareMountAddon:WeightedRandomSelect(favoriteMountsWithRarities)
     for i, mountWithRarity in ipairs(favoriteMountsWithRarities) do
         searchWeight = searchWeight + mountWithRarity.invertedRarity
         if (selectedWeight < searchWeight) then
-            return i
+            return i, (mountWithRarity.invertedRarity / totalWeight)
         end
     end
 
-    return nil
+    return nil, nil
 end
 
-function RandomRareMountAddon:DetermineRandomFavoriteMount()
-    local favoriteMounts = RandomRareMountAddon:GetFavoriteMounts()
-
+---@param mountList ShortMountInfo[]
+function RandomRareMountAddon:DetermineRandomMount(mountList)
     ---@type { mountInfo: ShortMountInfo, rarity: number, invertedRarity: number }[]
-    local favoriteMountsWithRarities = {}
+    local mountsWithRarities = {}
 
-    for i, mountInfo in ipairs(favoriteMounts) do
+    for i, mountInfo in ipairs(mountList) do
         local rarity = libMountsRarity:GetRarityByID(mountInfo.mountId)
 
         if (rarity ~= nil) then
-            table.insert(favoriteMountsWithRarities, {
+            table.insert(mountsWithRarities, {
                 mountInfo = mountInfo,
                 rarity = rarity,
                 invertedRarity = ( 1 / rarity )
@@ -86,39 +110,40 @@ function RandomRareMountAddon:DetermineRandomFavoriteMount()
         end
     end
 
-    local randomIndex = RandomRareMountAddon:WeightedRandomSelect(favoriteMountsWithRarities)
+    local randomIndex, chance = self:WeightedRandomSelect(mountsWithRarities)
     if (randomIndex == nil) then
-        return nil
+        return nil, nil
     end
 
-    return favoriteMountsWithRarities[randomIndex]
+    return mountsWithRarities[randomIndex], chance
 end
 
-function RandomRareMountAddon:SummonRandomFavoriteMount()
-    if ( libMountsRarity == nil or libMountsRarity.GetData() == nil ) then
-        self:Print("MountsRarity addon not loaded.")
-        return nil
-    end
-
-    local mount = RandomRareMountAddon:DetermineRandomFavoriteMount()
+---@type ShortMountInfo[]
+function RandomRareMountAddon:SummonRandomMount(mountList)
+    local mount, chance = self:DetermineRandomMount(mountList)
     if (mount == nil) then
         self:Print("Could not determine random mount.")
         return
     end
 
-    self:Print("Mount " .. mount.mountInfo.name .. " is owned by " .. string.format("%.02f", mount.rarity) .. "% of players.")
+    local owningPercentage = string.format("%.02f", mount.rarity)
+    local chancePercentage = string.format("%.02f", chance * 100)
+    self:Print(mount.mountInfo.name .. " owned by " .. owningPercentage .. "%. Chance to summon: " .. chancePercentage .. "%.")
+
     C_MountJournal.SummonByID(mount.mountInfo.mountId)
 end
 
 function RandomRareMountAddon:SlashCommands(args)
     local arg1 = self:GetArgs(args, 1)
 
+    ---@type ShortMountInfo[]
+    local potentialMounts = {}
     if arg1 and string.upper(arg1) == "FAVORITES" then
-        self:SummonRandomFavoriteMount()
+        potentialMounts = self:GetFavoriteMounts()
     else
-        -- TODO: implement
-        self:Print("Random across all mounts is not yet implemented.")
+        potentialMounts = self:GetAllMounts()
     end
+    self:SummonRandomMount(potentialMounts)
 end
 
 -- Lifecycle events
